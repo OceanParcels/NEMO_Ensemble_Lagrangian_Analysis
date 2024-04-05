d0db0bca146535e55e63d100acf454b6072e88ce
cpierard <c.m.pierard@uu.nl>
2024-04-05 12:18:11 +0200

    sample vel for correlations

--- simulations/ensemble_Member_simulation.py
+++ simulations/ensemble_Member_simulation.py
@@ -1,5 +1,5 @@
 #%%
-from parcels import FieldSet, ParticleSet, AdvectionRK4_3D, AdvectionRK4, ParticleFile, JITParticle, StatusCode
+from parcels import FieldSet, ParticleSet, AdvectionRK4_3D, AdvectionRK4, ParticleFile, JITParticle, StatusCode, Variable
 import numpy as np
 from glob import glob
 from datetime import timedelta as delta
@@ -15,6 +15,8 @@ member = args.member
 year = args.year
 
 print(year, member)
+
+# Import Fieldset
 
 data_path = '/storage/shared/oceanparcels/input_data/NEMO_Ensemble/'
 ufiles = sorted(glob(f"{data_path}NATL025-CJMCYC3.{member:03d}-S/1d/{year}/NATL025*U.nc"))
@@ -35,6 +37,7 @@ dimensions = {'U': {'lon': 'glamf', 'lat': 'gphif', 'depth': 'depthw', 'time': '
 
 fieldset = FieldSet.from_nemo(filenames, variables, dimensions, netcdf_decodewarning=False)
 
+# Declare the ParticleSet
 step=1/32.
 min_lon = -74
 max_lon = -72
@@ -49,17 +52,42 @@ X, Y, Z = np.meshgrid(np.arange(min_lon, max_lon, step),
 n_particles = len(X.flatten())
 start_times = [np.datetime64(f'{year}-01-02')]*n_particles
 
-pset = ParticleSet(fieldset, JITParticle, lon=X, lat=Y, depth=Z, time=start_times)
+class EnsembleParticle(JITParticle):
+    """
+    Particle class definition with additional variables
+    """
+    # dynamic variables
+    u = Variable('u', dtype=np.float32, initial=0)
+    v = Variable('v', dtype=np.float32, initial=0)
+    w = Variable('w', dtype=np.float32, initial=0)
+    
+    # distance_from_x0 = Variable('distance', dtype= np.float32, initial=0)
 
+pset = ParticleSet(fieldset, EnsembleParticle, lon=X, lat=Y, depth=Z, time=start_times)
+
+# Declare
 def KeepInOcean(particle, fieldset, time):
     if particle.state == StatusCode.ErrorThroughSurface:
         particle_ddepth = 1.0
         particle.state = StatusCode.Success
+        
+def SampleField(particle, fieldset, time):
+    """
+    Sample the fieldset at the particle location and store it in the
+    particle variable.
+    """
+    (u1, v1, w1) = fieldset.UVW.eval(time, particle.depth, particle.lat, particle.lon, applyConversion=False)
+    particle.u = u1
+    particle.v = v1
+    particle.w = w1
 
-outfile = f"/storage/shared/oceanparcels/output_data/data_Claudio/NEMO_Ensemble/{year}/PGS_{year}_{member:03d}.zarr"
+    
+
+# outfile = f"/storage/shared/oceanparcels/output_data/data_Claudio/NEMO_Ensemble/{year}/PGS_{year}_{member:03d}.zarr"
+outfile = f"/storage/shared/oceanparcels/output_data/data_Claudio/NEMO_Ensemble/test_{year}_{member:03d}.zarr"
 pfile = ParticleFile(outfile, pset, outputdt=delta(days=1), chunks=(len(pset), 1))
 
-pset.execute([AdvectionRK4_3D, KeepInOcean], 
+pset.execute([AdvectionRK4_3D, KeepInOcean, SampleField], 
              dt=delta(hours=1), 
              output_file=pfile)
 
