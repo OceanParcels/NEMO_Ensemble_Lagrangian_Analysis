6e3574f35107383d2a32635cd7f51098707798dd
cpierard <c.m.pierard@uu.nl>
2024-07-05 17:59:46 +0200

    ..

--- analysis/submit_analysis.sh
+++ analysis/submit_analysis.sh
@@ -4,8 +4,8 @@
 #SBATCH -p normal           # request normal partition, job takes > 1 hour (this line can also be left out because 'normal' is the default)  
 #SBATCH -t 120:00:00         # time in hh:mm:ss you want to reserve for the job
 #SBATCH -n 1               # the number of cores you want to use for the job, SLURM automatically determines how many nodes are needed
-#SBATCH -o logs/SingleMember.%j.o  # the name of the file where the standard output will be written to. %j will be the jobid determined by SLURM
-#SBATCH -e logs/SingleMember.%j.e  # the name of the file where potential errors will be written to. %j will be the jobid determined by SLURM
+#SBATCH -o logs/.%j.o  # the name of the file where the standard output will be written to. %j will be the jobid determined by SLURM
+#SBATCH -e logs/.%j.e  # the name of the file where potential errors will be written to. %j will be the jobid determined by SLURM
 #SBATCH --mail-user=c.m.pierard@uu.nl
 #SBATCH --mail-type=ALL
 
