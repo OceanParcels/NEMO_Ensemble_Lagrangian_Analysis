549bbeda1384e1eacdd2926e91263d5f47b375f4
cpierard <c.m.pierard@uu.nl>
2024-10-01 23:49:18 +0200

    Time average cross entropy plot - henlo Mike-

---
+++ plot_scripts/Cross_entropy_in_time.py
@@ -0,0 +1,190 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+# import cartopy
+from tqdm import tqdm
+# import pickle
+import pandas as pd
+import seaborn as sns
+import cmocean.cm as cmo
+import sys
+sys.path.append('../functions')
+import hexbin_functions as hexfunc
+
+
+def cross_entropy(P, Q, axis=0):
+    # Cross entropy H_p(q) = - sum_x p(x) log q(x)
+    # P is the true distribution, Q is the estimated distribution.
+    # Pdf = Pdf / np.nansum(Pdf)  # Normalize Pdf to sum to 1, ignoring NaNs
+    # Replace zeros with a very small number to avoid log(0)
+    P_safe = np.where(P > 0, P, np.finfo(float).eps)
+    return -np.nansum(Q * np.log2(P_safe), axis=axis)
+
+def marginal_entropy(P, axis=0):
+    # Shannon entropy
+    # Pdf = Pdf / np.nansum(Pdf)  # Normalize Pdf to sum to 1, ignoring NaNs
+    # Replace zeros with a very small number to avoid log(0)
+    P_safe = np.where(P > 0, P, np.finfo(float).eps)
+    return -np.nansum(P_safe * np.log2(P_safe), axis=axis)
+
+def KLDivergence(P, Q, axis=0):
+    # Kullback-Leibler divergence D_KL(P||Q)
+    # P is the true distribution, Q is the estimated distribution.
+    return cross_entropy(P, Q, axis=axis) - marginal_entropy(Q, axis=axis)
+
+location = 'Cape_Hatteras'
+delta_r = 0.1
+subset = 1
+
+base_path = "/storage/shared/oceanparcels/output_data/data_Claudio/NEMO_Ensemble/analysis/prob_distribution/"
+
+file_path_AX = base_path + f"{location}_all_long/P_dr{delta_r*100:03.0f}_all_s{subset:03d}.nc"
+P_AX = xr.open_dataset(file_path_AX)
+
+hex_grid = hexfunc.int_to_hex(P_AX.hexint.values)
+hexbin_grid = hexfunc.hexGrid(hex_grid, h3_res=3)
+
+
+#%% KLD
+N_members = 10
+
+time_length = 2189 - 7*20
+time_range = np.arange(0, time_length)
+
+KLD_ALL_mean = {}
+KLD_ALL_std = {}
+
+for delta_ref in [0.1, 2., 4, 20]:
+    
+    KLDivergence_mean = {}
+    KLDivergence_std = {}
+    
+    for set in [0.1, 2., 4, 20]:
+        print(f'Processing set P: {delta_ref}, Q:{set}')
+
+        _KLD = np.zeros((N_members**2, time_length))
+
+        l = 0
+        for member in tqdm(range(1, N_members+1)):
+            for subset_ref in range(1, N_members+1):
+                # print(f'member {member} and subset {subset_ref}')
+
+                if delta_ref in [0.1, 1., 2.]:
+                    file_path_ref = base_path + f"{location}_all_long/P_dr{delta_ref*100:03.0f}_all_s{subset_ref:03d}.nc"
+                elif delta_ref in [4, 12, 20]:
+                    file_path_ref = base_path + f"{location}_all_long/P_W{delta_ref:02d}_all_s{subset_ref:03d}.nc"
+                
+                P_ref = xr.open_dataset(file_path_ref)
+                P_ref = P_ref.sortby('hexint')
+
+                if set in [0.1, 1., 2.]:
+                    file_path_Q = base_path + f"{location}_spatial_long/P_dr{set*100:03.0f}_m{member:03d}.nc"
+                elif set in [4, 12, 20]:
+                    file_path_Q = base_path + f"{location}_temporal_long/P_W{set:01d}_m{member:03d}.nc"
+                
+                P_Q = xr.open_dataset(file_path_Q)
+                P_Q = P_Q.sortby('hexint')
+
+                _KLD[l, :] = KLDivergence(P_ref['probability'], P_Q['probability'], axis=0)[:time_length]
+                l+=1
+                
+        avg_KLD = np.mean(_KLD, axis=0)
+        std_KLD = np.std(_KLD, axis=0)
+        
+        KLDivergence_mean[set] = avg_KLD
+        KLDivergence_std[set] = std_KLD
+
+    KLD_ALL_mean[delta_ref] = KLDivergence_mean
+    KLD_ALL_std[delta_ref] = KLDivergence_std
+
+#%%
+fig, axs = plt.subplots(2, 2, figsize=(10, 8), sharex=True, sharey=True)
+# fig.subplots_adjust(hspace=0.4, wspace=0.4)
+
+# keys = list(KLD_ALL_mean[0.1].keys())
+labels = ['A', 'B', 'C', 'D', 'E', 'F']
+
+axs = axs.ravel()
+
+for i, key in enumerate(KLD_ALL_mean.keys()):
+    
+    lss = [(0, (1, 1)), '-.', '--', (0, (3, 1, 1, 1, 1, 1))]
+    colors = ['mediumblue', 'teal', 'darkred', 'black']
+    
+    ax = axs[i]
+    for k, set in enumerate([0.1, 2., 4, 20]):
+        if set > 2:
+            labelz = f'{set:01d} weeks'
+        else:
+            labelz = f'$\delta_r = {set}^o$'
+        
+        ax.fill_between(time_range, KLD_ALL_mean[key][set] - KLD_ALL_std[key][set], 
+                        KLD_ALL_mean[key][set] + KLD_ALL_std[key][set], alpha=0.2, 
+                        color=colors[k])
+        ax.plot(time_range, KLD_ALL_mean[key][set], label=labelz, color=colors[k], 
+                linestyle=lss[k], linewidth=2)
+    
+    if key > 2:
+        labeltt = f'Mixture {key:01d} weeks'
+    else:
+        labeltt = f'Mixture $\delta_r = {key}^o$'
+        
+    ax.text(0.05, 0.9, f'$\mathbf{{{labels[i]}}}$  Ref.: {labeltt}', fontsize=14, transform=ax.transAxes)
+
+    # ax.semilogx()
+    ax.legend()
+    # ax.grid()
+    ax.set_xlim(0, time_length)
+    ax.set_ylim(0, 20)
+
+axs[2].set_xlabel('Time (days)')
+axs[3].set_xlabel('Time (days)')
+axs[0].set_ylabel('KL Divergence (bits)')
+axs[2].set_ylabel('KL Divergence (bits)')
+
+plt.tight_layout()
+
+plt.savefig('../figs/KLDivergence_subplots.png', dpi=300)
+# %%
+DF = {}
+
+for i, key in enumerate(KLD_ALL_mean.keys()):
+    _kld = np.zeros(4)
+    
+    for k, set in enumerate([0.1, 2., 4, 20]):
+        
+        _kld[k] = np.mean(KLD_ALL_mean[key][set])
+        
+    if key > 2:
+        labeltt = f'Mix. {key:01d} weeks'
+    else:
+        labeltt = f'Mix. $\delta_r = {key}^o$'
+        
+    DF[labeltt] = _kld
+
+DF = pd.DataFrame(DF)
+DF.set_index([[r'$\delta_r = 0.1^o$', r'$\delta_r = 2^o$', 'Mix 4 weeks', 'Mix 20 weeks']], inplace=True)
+
+# %% Kullback-Leibler divergence plot 
+fig, ax = plt.subplots(figsize=(6, 5))
+
+cmmap = cmo.amp
+
+sns.heatmap(DF.T, annot=True, fmt=".3f", cmap=cmmap, ax=ax, cbar=True)
+
+# Rotate y tick labels 90 degrees
+ax.set_yticklabels(ax.get_yticklabels(), rotation=90, ha='center', fontsize=9)
+# Rotate x tick labels 15 degrees
+ax.set_xticklabels(ax.get_xticklabels(), rotation=15, ha='center', fontsize=9)
+
+# Add colorbar label
+cbar = ax.collections[0].colorbar
+cbar.set_label('Time Average KL Divergence, $D_{P_{Mix}}(P_i)$ (bits)')
+
+plt.tight_layout()
+plt.show()
+
+fig.savefig("../figs/FigX_Time_Average_KLDivergence.png", dpi=300)
+
+# %%
--- plot_scripts/fig3-4-Representation_entropy.py
+++ plot_scripts/fig3-4-Representation_entropy.py
@@ -2,13 +2,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import xarray as xr
-import cartopy
-from tqdm import tqdm
-import pickle
+# import cartopy
+# from tqdm import tqdm
+# import pickle
 
 import sys
 sys.path.append('../functions')
 import hexbin_functions as hexfunc
+
 
 location = 'Cape_Hatteras'
 delta_r = 0.1
@@ -27,8 +28,8 @@ mixture_entropy_space = {}
 std_mixture_entropy_space = {}
 
 for delta_r in [0.1, 2]:
-    entropy_all = np.zeros((14, len(P_AX['entropy'].values)))
-    for i in range(1, 15):
+    entropy_all = np.zeros((51, len(P_AX['entropy'].values)))
+    for i in range(1, 51):
         
         file_path_AX = f"/storage/shared/oceanparcels/output_data/data_Claudio/NEMO_Ensemble/analysis/prob_distribution/{location}_all_long/P_dr{delta_r*100:03.0f}_all_s{i:03d}.nc"
         P_AX = xr.open_dataset(file_path_AX)
@@ -44,9 +45,9 @@ mixture_entropy_time = {}
 std_mixture_entropy_time = {}
 
 for week in [4, 20]:
-    entropy_all = np.zeros((13, len(P_AX['entropy'].values)))
+    entropy_all = np.zeros((50, len(P_AX['entropy'].values)))
     
-    for i in range(1, 14):
+    for i in range(1, 50):
         
         file_path_AX = f"/storage/shared/oceanparcels/output_data/data_Claudio/NEMO_Ensemble/analysis/prob_distribution/{location}_all_long/P_W{week:02d}_all_s{i:03d}.nc"
         P_AX = xr.open_dataset(file_path_AX)
@@ -72,6 +73,23 @@ for i, delta_r in enumerate([0.1, 1, 2]):
     entropies_space_mean[delta_r] = np.mean(_entropy, axis=0)
     entropies_space_std[delta_r] = np.std(_entropy, axis=0)
 
+
+# %%%%%%%%%%%%%%%%%%%%%% TEMPORAL Analysis %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+################################################################################
+week_range = [4, 12, 20]
+
+entropies_time_mean = {}
+entropies_time_std = {}
+
+for week in week_range:
+    _entropy = np.zeros((50, len(P_AX['entropy'].values)))
+    for i, member in enumerate(range(1, 51)):
+        file_path = f"/storage/shared/oceanparcels/output_data/data_Claudio/NEMO_Ensemble/analysis/prob_distribution/{location}_temporal_long/P_W{week:01d}_m{member:03d}.nc"
+        P_m = xr.open_dataset(file_path)
+        _entropy[i, :] = P_m['entropy'].values
+        
+    entropies_time_mean[week] = np.mean(_entropy, axis=0)
+    entropies_time_std[week] = np.std(_entropy, axis=0)
 
 # %% spatial plot of the mean entropy
 time_range = np.arange(0, 2189)
@@ -103,23 +121,6 @@ ax.set_xlabel('Time (days)')
 ax.grid()
 plt.savefig('../figs/Fig3-Spatial-Representation_entropy.png', dpi=300)
 
-
-# %%%%%%%%%%%%%%%%%%%%%% TEMPORAL Analysis %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-################################################################################
-week_range = [4, 12, 20]
-
-entropies_time_mean = {}
-entropies_time_std = {}
-
-for week in week_range:
-    _entropy = np.zeros((50, len(P_AX['entropy'].values)))
-    for i, member in enumerate(range(1, 51)):
-        file_path = f"/storage/shared/oceanparcels/output_data/data_Claudio/NEMO_Ensemble/analysis/prob_distribution/{location}_temporal_long/P_W{week:01d}_m{member:03d}.nc"
-        P_m = xr.open_dataset(file_path)
-        _entropy[i, :] = P_m['entropy'].values
-        
-    entropies_time_mean[week] = np.mean(_entropy, axis=0)
-    entropies_time_std[week] = np.std(_entropy, axis=0)
 
 # %%%%%%%%%%%%%%%%%%%%%%%%% Temporal plot of the mean entropy %%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -162,3 +163,69 @@ plt.savefig('../figs/Fig4-Temporal-Representation_entropy.png', dpi=300)
 
 
 #%%
+# %% Combined spatial and temporal plot of the mean entropy
+time_range = np.arange(0, 2189)
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(11, 4), sharey=True)
+
+# Spatial plot
+lss = [(0, (1, 1)), '--', '-.']
+i = 0
+colors_space = ['mediumblue', 'blueviolet', 'teal']
+for delta_r in [0.1, 1., 2.]:
+    ax1.plot(time_range, entropies_space_mean[delta_r],  
+             label=f'$\delta_r = {delta_r}^o$', linestyle=lss[i], linewidth=2, color=colors_space[i])
+    ax1.fill_between(time_range, entropies_space_mean[delta_r] - entropies_space_std[delta_r], 
+                     entropies_space_mean[delta_r] + entropies_space_std[delta_r], 
+                     alpha=0.2, color=colors_space[i])
+    i += 1
+
+ax1.plot(time_range, mixture_entropy_space[0.1], ls='-', color='black', label=r'Mixture: $\delta_r = 0.1^o$')
+ax1.plot(time_range, mixture_entropy_space[2], ls=(0, (3, 1, 1, 1)), color='black', label=r'Mixture: $\delta_r = 2.0^o$')
+
+ax1.set_xlim(1, 2189)
+ax1.set_ylim(0., 10.5)
+ax1.semilogx()
+ax1.legend(shadow=True)
+ax1.set_ylabel('Entropy (bits)')
+ax1.set_xlabel('Time (days)')
+ax1.grid()
+ax1.text(0.95, 0.05, 'A', transform=ax1.transAxes, fontsize=16, fontweight='bold', va='bottom', ha='right')
+
+# Temporal plot
+lss = [(0, (1, 1)), '--', '-.', (0, (3, 1, 1, 1, 1, 1))]
+i = 0
+colors_time = ['darkred', 'orangered', 'orange']
+
+for week in week_range:
+    chop_time = len(P_m['time'].values) - week*7
+    ax2.plot(time_range[:chop_time], entropies_time_mean[week][:chop_time],  
+             label=f'{week} weeks release', linestyle=lss[i], linewidth=2, color=colors_time[i])
+    ax2.fill_between(time_range[:chop_time], entropies_time_mean[week][:chop_time] - entropies_time_std[week][:chop_time], 
+                     entropies_time_mean[week][:chop_time] + entropies_time_std[week][:chop_time],
+                     alpha=0.2, color=colors_time[i])
+    i += 1
+
+ax2.plot(time_range[:2189-4*7], mixture_entropy_time[4][:2189-4*7], ls=(0, (3, 1, 1, 3)), 
+         color='black', label='Mixture: 4 weeks')
+
+# ax2.plot(time_range[:2189-20*7], mixture_entropy_time[20][:2189-20*7], ls=(0, (3, 1, 1, 5)), 
+#          color='black', label='Mixture: 20 weeks')
+
+# ax2.plot(time_range[:730-16*7], mixture_entropy_time[16][:730-16*7], ls='-', 
+#          color='black', label='Mixture: 16 weeks')
+
+ax2.plot(time_range, mixture_entropy_space[0.1], ls='-', color='black', label=r'Mixture: $\delta_r = 0.1^o$')
+
+ax2.set_xlim(1, 2189)
+ax2.set_ylim(0., 10.5)
+ax2.semilogx()
+ax2.legend(shadow=True)
+ax2.set_xlabel('Time (days)')
+ax2.grid()
+ax2.text(0.95, 0.05, 'B', transform=ax2.transAxes, fontsize=16, fontweight='bold', va='bottom', ha='right')
+
+plt.tight_layout()
+plt.savefig('../figs/Fig3-4-Combined-Representation_entropy.png', dpi=300)
+
+# %%
