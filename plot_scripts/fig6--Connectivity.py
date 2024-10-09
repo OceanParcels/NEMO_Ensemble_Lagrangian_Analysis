48c48ad63dd86137dd368eff0ddf3017d5060821
cpierard <c.m.pierard@uu.nl>
2024-10-10 00:31:55 +0200

    ecdf counts

--- plot_scripts/fig6--Connectivity.py
+++ plot_scripts/fig6--Connectivity.py
@@ -58,7 +58,7 @@ for delta_r in [0.1, 1., 2.]:
     
     sns.kdeplot(all_space[delta_r]["median_time"]/365, ax=ax[1], label=f"Spatial dr{delta_r}", clip=(0, 6),
                 fill=False, color=colors_space[j], linestyle=ls_space[j])
-    sns.kdeplot(all_space[delta_r]["min_time"]/365, ax=ax[2], label=f"Spatial dr{delta_r}", clip=(0, 6),
+    sns.kdeplot(all_space[delta_r]["median_depth"], ax=ax[2], label=f"Spatial dr{delta_r}", clip=(0, 1000),
                 fill=False, color=colors_space[j], linestyle=ls_space[j])
     j += 1
 
@@ -72,7 +72,7 @@ for week in [4, 12, 20]:
     
     sns.kdeplot(all_temp[week]["median_time"]/365, ax=ax[4], label=f"Temporal W{week}", clip=(0, 6),
                 fill=False, color=colors_temp[j], linestyle=ls_time[j])
-    sns.kdeplot(all_temp[week]["min_time"]/365, ax=ax[5], label=f"Temporal W{week}", clip=(0, 6),
+    sns.kdeplot(all_temp[week]["median_depth"], ax=ax[5], label=f"Temporal W{week}", clip=(0, 1000),
                 fill=False, color=colors_temp[j], linestyle=ls_time[j])
     j += 1
 
@@ -81,6 +81,13 @@ labels = ['A', 'B', 'C', 'D', 'E', 'F']
 for i, label in enumerate(labels):
     ax[i].text(0.05, 0.95, label, transform=ax[i].transAxes, fontsize=12, fontweight='bold', va='top', ha='left')
 
+ax[0].set_xlim(0, 6500)
+ax[3].set_xlim(0, 6500)
+ax[1].set_xlim(0, 6)
+ax[4].set_xlim(0, 6)
+ax[2].set_xlim(0, 200)
+ax[5].set_xlim(0, 200)
+
 ax[0].set_xlabel("Counts")
 ax[0].set_ylabel("Density")
 ax[0].legend(fontsize=7, shadow=False)
@@ -88,7 +95,7 @@ ax[0].legend(fontsize=7, shadow=False)
 ax[1].set_xlabel("Median Drift Time (years)")
 ax[1].set_ylabel("Density")
 
-ax[2].set_xlabel("Minimum Drift Time (years)")
+ax[2].set_xlabel("Median Depth (m)")
 ax[2].set_ylabel("Density")
 
 ax[3].set_xlabel("Counts")
@@ -98,7 +105,7 @@ ax[3].legend(fontsize=7, shadow=False)
 ax[4].set_xlabel("Median Drift Time (years)")
 ax[4].set_ylabel("Density")
 
-ax[5].set_xlabel("Minimum Drift Time (years)")
+ax[5].set_xlabel("Median Depth (m)")
 ax[5].set_ylabel("Density")
 
 # set a title
@@ -129,7 +136,7 @@ for delta_r in [0.1, 1., 2.]:
                 fill=False, color=colors_space[j], linestyle=ls_space[j])
     # sns.kdeplot(all_mix_space[delta_r]["std_time"]/365, ax=ax[5], label=f"Mix. $\delta_r = {delta_r}^o$", clip=(0, 6),
     #             fill=False, color=colors_space[j], linestyle=ls_space[j])
-    sns.kdeplot(all_mix_space[delta_r]["min_time"]/365, ax=ax[2], label=f"Mix. $\delta_r = {delta_r}^o$", clip=(0, 6),
+    sns.kdeplot(all_mix_space[delta_r]["median_depth"], ax=ax[2], label=f"Mix. $\delta_r = {delta_r}^o$", clip=(0, 1000),
                 fill=False, color=colors_space[j], linestyle=ls_space[j])
     j += 1
 
@@ -145,7 +152,7 @@ for week in [4, 12, 20]:
                 fill=False, color=colors_temp[j], linestyle=ls_time[j])
     # sns.kdeplot(all_mix_temp[week]["std_time"]/365, ax=ax[2], label=f"Mix. {week} weeks", clip=(0, 6),
     #             fill=False, color=colors_temp[j], linestyle=ls_time[j])
-    sns.kdeplot(all_mix_temp[week]["min_time"]/365, ax=ax[5], label=f"Mix. {week} weeks", clip=(0, 6),
+    sns.kdeplot(all_mix_temp[week]["median_depth"], ax=ax[5], label=f"Mix. {week} weeks", clip=(0, 1000),
                fill=False, color=colors_temp[j], linestyle=ls_time[j])
     j += 1
 
@@ -155,17 +162,24 @@ for i, label in enumerate(labels):
     ax[i].text(0.05, 0.95, label, transform=ax[i].transAxes, fontsize=12, fontweight='bold', va='top', ha='left')
     
 
+ax[0].set_xlim(0, 6500)
+ax[3].set_xlim(0, 6500)
+ax[1].set_xlim(0, 6)
+ax[4].set_xlim(0, 6)
+ax[2].set_xlim(0, 200)
+ax[5].set_xlim(0, 200)
+
 ax[0].set_xlabel("Counts")
 ax[0].set_ylabel("Density")
 ax[0].legend(fontsize=7, shadow=False)
 # ax[0].grid()
 
-ax[1].set_xlabel("Median Drift Time (years)")
+ax[1].set_xlabel("Median Particle Age (years)")
 ax[1].set_ylabel("Density")
 # ax[1].grid()
 
 # ax[2].set_xlabel("STD Drift Time (years)")
-ax[2].set_xlabel("Minimum Drift Time (years)")
+ax[2].set_xlabel("Median Depth (m)")
 ax[2].set_ylabel("Density")
 # ax[2].grid()
 
@@ -174,12 +188,12 @@ ax[3].set_ylabel("Density")
 ax[3].legend(fontsize=7, shadow=False)
 # ax[3].grid()
 
-ax[4].set_xlabel("Median Drift Time (years)")
+ax[4].set_xlabel("Median Particle Age (years)")
 ax[4].set_ylabel("Density")
 # ax[4].grid()
 
 # ax[5].set_xlabel("STD Drift Time (years)")
-ax[5].set_xlabel("Minimum Drift Time (years)")
+ax[5].set_xlabel("Median Depth (m)")
 ax[5].set_ylabel("Density")
 # ax[5].grid()
 
@@ -260,24 +274,184 @@ labels = ['A', 'B', 'C', 'D']
 for i, label in enumerate(labels):
     ax[i].text(0.05, 0.95, label, transform=ax[i].transAxes, fontsize=12, fontweight='bold', va='top', ha='left')
 
+
+ax[0].set_xlim(1500, 3500)
+ax[3].set_xlim(1500, 3500)
+ax[1].set_xlim(2, 4)
+ax[4].set_xlim(2, 4)
+ax[2].set_xlim(0, 60)
+ax[5].set_xlim(0, 60)
+
 ax[0].set_xlabel("Counts")
 ax[0].set_ylabel("Density")
 ax[0].legend(fontsize=7)
 
-ax[1].set_xlabel("Median Drift Time (years)")
+ax[1].set_xlabel("Median Particle Age (years)")
 ax[1].set_ylabel("Density")
 
 ax[2].set_xlabel("Counts")
 ax[2].set_ylabel("Density")
 ax[2].legend(fontsize=7)
 
-ax[3].set_xlabel("Median Drift Time (years)")
+ax[3].set_xlabel("Median Particle Age (years)")
 ax[3].set_ylabel("Density")
 
 plt.tight_layout()
 # save the figure
 plt.savefig("../figs/Figx-Connect_Comparisson" + criterium_string + ".png", dpi=300)
 
+#%% ECDFs of the counts and median time for temporal and spatial members
+# 
+fig, ax = plt.subplots(2, 2, figsize=(8, 6))
+ax = ax.flatten()
+# Plot CDFs for percentage of subpolar trajectories
+
+colors_space = ["midnightblue", "blueviolet", "teal"]
+ls_space = [(0, (1, 1)), '--', '-.']
+
+for j, delta_r in enumerate([0.1, 1., 2.]):
+    sns.ecdfplot(all_space[delta_r]["counts"], ax=ax[0], label=f"$\delta_r = {delta_r}^o$", 
+                 color=colors_space[j], linestyle=ls_space[j])
+    sns.ecdfplot(all_space[delta_r]["median_time"]/365, ax=ax[1], label=f"Spatial dr{delta_r}", 
+                 color=colors_space[j], linestyle=ls_space[j])
+
+for j, dr_ref in enumerate([0.1, 1., 2.]):
+    mean_counts = all_mix_space[dr_ref]["counts"].mean()
+    std_counts = all_mix_space[dr_ref]["counts"].std()
+
+    ax[0].fill_betweenx([0, 1], mean_counts-std_counts, mean_counts+std_counts,
+                        color=colors_space[j], alpha=0.3, label=f"Mix. $\delta_r = {dr_ref}^o$",
+                        edgecolor='none')
+    ax[0].set_xlim(0, 6500)
+    
+    mean_median_time = all_mix_space[dr_ref]["median_time"].mean()/365
+    std_median_time = all_mix_space[dr_ref]["median_time"].std()/365
+
+    ax[1].axvline(mean_median_time, color=colors_space[j], linestyle='-', 
+                  label=f"Mix. $\delta_r = {dr_ref}^o$", alpha=0.3)
+    ax[1].set_xlim(1, 6)
+
+colors_temp = ["darkred", "orangered", "orange"]
+ls_time = [(0, (1, 1)), '--', '-.', (0, (3, 1, 1, 1, 1, 1))]
+
+for j, week in enumerate([4, 12, 20]):
+    sns.ecdfplot(all_temp[week]["counts"], ax=ax[2], label=f"{week} weeks", color=colors_temp[j], linestyle=ls_time[j])
+    sns.ecdfplot(all_temp[week]["median_time"]/365, ax=ax[3], label=f"Temporal W{week}", color=colors_temp[j], linestyle=ls_time[j])
+
+for j, dr_ref in enumerate([4, 12, 20]):
+    mean_counts = all_mix_temp[dr_ref]["counts"].mean()
+    std_counts = all_mix_temp[dr_ref]["counts"].std()
+
+    ax[2].fill_betweenx([0, 1], mean_counts-std_counts, mean_counts+std_counts,
+                        color=colors_temp[j], alpha=0.3, label=f"Mix. {dr_ref} weeks", edgecolor='none')
+    ax[2].set_xlim(0, 6500)
+    
+    mean_median_time = all_mix_temp[dr_ref]["median_time"].mean()/365
+    std_median_time = all_mix_temp[dr_ref]["median_time"].std()/365
+
+    ax[3].axvline(mean_median_time, color=colors_temp[j], linestyle='-', 
+                  label=f"Mix. $\delta_r = {dr_ref}^o$", alpha=0.3)
+    ax[3].set_xlim(1, 6)
+
+# Add labels 'A', 'B', 'C', 'D' for each subplot in the top left corner
+labels = ['A', 'B', 'C', 'D']
+for i, label in enumerate(labels):
+    ax[i].text(0.05, 0.95, label, transform=ax[i].transAxes, fontsize=12, fontweight='bold', va='top', ha='left')
+
+ax[0].set_xlabel("Counts")
+ax[0].set_ylabel("CDF")
+ax[0].legend(fontsize=7)
+ax[0].grid(True)
+
+ax[1].set_xlabel("Median Drift Time (years)")
+ax[1].set_ylabel("ECDF")
+ax[1].grid(True)
+
+ax[2].set_xlabel("Counts")
+ax[2].set_ylabel("ECDF")
+ax[2].legend(fontsize=7, loc='lower right')
+ax[2].grid(True)
+
+ax[3].set_xlabel("Median Drift Time (years)")
+ax[3].set_ylabel("ECDF")
+ax[3].grid(True)
+
+plt.tight_layout()
+# save the figure
+
+plt.savefig("../figs/Figx-ECDF_Comparisson" + criterium_string + ".png", dpi=300)
+
+#%% COMpute ECDF curver for all_temp and all_space with and ecdf function writen by you
+def ecdf(data):
+    x = np.sort(data)
+    y = np.arange(1, len(x)+1) / len(x)
+    return x, y
+
+probss = {}
+
+for dr_ref in [0.1, 1., 2.]:
+    mean = all_mix_space[dr_ref]["counts"].mean()
+    
+    p = np.zeros(6)
+    
+    for i, delta_r in enumerate([0.1, 1., 2.]):
+        X, Y = ecdf(all_space[delta_r]["counts"])
+
+        idx = np.where(X >= mean)[0][0]
+        
+        p[i] = Y[idx]
+        
+    for i, week in enumerate([4, 12, 20], start=3):
+        X, Y = ecdf(all_temp[week]["counts"])
+
+        idx = np.where(X >= mean)[0][0]
+        
+        p[i] = Y[idx]
+        
+    probss[f'Mix $\delta_r = {dr_ref}^o$'] = p
+    
+for week_ref in [4, 12, 20]:
+    mean = all_mix_temp[week_ref]["counts"].mean()
+    
+    p = np.zeros(6)
+    
+    for i, delta_r in enumerate([0.1, 1., 2.]):
+        X, Y = ecdf(all_space[delta_r]["counts"])
+
+        idx = np.where(X >= mean)[0][0]
+        
+        p[i] = Y[idx]
+        
+    for i, week in enumerate([4, 12, 20], start=3):
+        X, Y = ecdf(all_temp[week]["counts"])
+
+        idx = np.where(X >= mean)[0][0]
+        
+        p[i] = Y[idx]
+        
+    probss[f'Mix {week_ref} weeks'] = p
+
+DF = pd.DataFrame(probss)
+DF.set_index([[r'$\delta_r = 0.1^o$', r'$\delta_r = 1^o$',  r'$\delta_r = 2^o$', '4 weeks', '12 weeks', '20 weeks']], inplace=True)
+
+fig, ax = plt.subplots(figsize=(6, 5))
+
+cmmap = "Purples_r"
+
+sns.heatmap(DF, annot=True, fmt=".2f", cmap=cmmap, ax=ax, cbar=True, vmin=0)
+
+# Rotate y tick labels 90 degrees
+ax.set_yticklabels(ax.get_yticklabels(), rotation=90, ha='center', fontsize=9)
+# Rotate x tick labels 15 degrees
+ax.set_xticklabels(ax.get_xticklabels(), rotation=15, ha='center', fontsize=9)
+
+# Add colorbar label
+cbar = ax.collections[0].colorbar
+cbar.set_label('Probability of having fewer particles than the Mixture')
+
+plt.tight_layout()
+plt.show()
+plt.savefig(f"../figs/Figx-ECDF_Probs.png", dpi=300)
 #%% Average the temporal and spatial members and mixtures and put the values in a dataframe
 
 zero_counts = np.zeros(3*4)
