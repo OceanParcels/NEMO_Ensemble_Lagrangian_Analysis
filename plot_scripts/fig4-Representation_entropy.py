Update figures, but found bug in normalization pf probabilities. To do fix single_member_statistics_diffusion.py

plot_scripts/fig4-Representation_entropy.py
@@ -95,7 +95,7 @@ for week in week_range:
 
 # %%%%%%%%%%%%%%%%%%%%%% Diffusion Analysis %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ################################################################################
-K_h_range = [10]
+K_h_range = [10, 1000]
 
 entropies_diff_mean = {}
 entropies_diff_std = {}
@@ -116,7 +116,7 @@ for K_h in K_h_range:
 # %% Combined spatial and temporal plot of the mean entropy
 time_range = np.arange(0, 2189)
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(11, 4), sharey=True)
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4), sharey=True)
 
 # Spatial plot
 lss = [(0, (1, 1)), '--', '-.']
@@ -133,25 +133,6 @@ for delta_r in [0.1, 1., 2.]:
 
 ax1.plot(time_range, mixture_entropy_space[0.1], ls='-', color='black', label=r'Mixture: $\delta_r = 0.1^o$')
 ax1.plot(time_range, mixture_entropy_space[2], ls=(0, (3, 1, 1, 1)), color='black', label=r'Mixture: $\delta_r = 2.0^o$')
-
-# diffusion
-K_h = 10
-# Create logarithmically spaced indices
-n_samples = 100  # Choose the number of points you want
-log_indices = np.unique(np.geomspace(1, len(time_range)-1, n_samples).astype(int))
-
-# Use these indices to sample your data
-time_sampled = time_range[log_indices]
-y_sampled = entropies_diff_mean[K_h][log_indices]
-
-# ax.plot(time_range[:], entropies_diff_mean[K_h][:],  
-#         label=r'$K_h = 10 \ m^2 s^{-1}$', linestyle=(0, (2, 2, 10, 2)) , linewidth=2, color=color_diff)
-
-ax1.fill_between(time_range[:], entropies_diff_mean[K_h][:] - entropies_diff_std[K_h][:], 
-                entropies_diff_mean[K_h][:] + entropies_diff_std[K_h][:],
-                alpha=0.2, color='grey')
-ax1.scatter(time_sampled, y_sampled,
-        label=r'$K_h = 10 \ m^2 s^{-1}$', color='k', marker='^', s=10)
 
 ax1.set_xlim(1, 2189)
 ax1.set_ylim(0., 10.5)
@@ -177,7 +158,6 @@ for week in week_range:
     i += 1
 
 
-
 ax2.plot(time_range, mixture_entropy_space[0.1], ls='-', color='black', label=r'Mixture: $\delta_r = 0.1^o$')
 ax2.plot(time_range[:2189-4*7], mixture_entropy_time[4][:2189-4*7], ls=(0, (3, 1, 1, 3)), 
          color='black', label='Mixture: 4 weeks')
@@ -193,36 +173,14 @@ ax2.set_xlabel('Particle Age (days)')
 ax2.grid()
 ax2.text(0.95, 0.05, 'B', transform=ax2.transAxes, fontsize=16, fontweight='bold', va='bottom', ha='right')
 
-plt.tight_layout()
-plt.savefig('../figs/Fig4_Combined-Representation_entropy.png', dpi=300)
-
-# %% With diffusion
-
-fig, ax = plt.subplots()
-
-# Spatial plot
-delta_r = 2.0
-ax.plot(time_range, entropies_space_mean[delta_r],  
-            label=f'$\delta_r = {delta_r}^o$', linestyle='-.', linewidth=2, color='teal')
-ax.fill_between(time_range, entropies_space_mean[delta_r] - entropies_space_std[delta_r], 
-                    entropies_space_mean[delta_r] + entropies_space_std[delta_r], 
-                    alpha=0.2, color="teal")
-
-
-# time
-week = 20
-chop_time = len(P_m['time'].values) - week*7
-ax.plot(time_range[:chop_time], entropies_time_mean[week][:chop_time],  
-        label=f'{week} weeks release', linestyle=(0, (3, 1, 1, 1, 1, 1)), linewidth=2, color='orange')
-ax.fill_between(time_range[:chop_time], entropies_time_mean[week][:chop_time] - entropies_time_std[week][:chop_time], 
-                entropies_time_mean[week][:chop_time] + entropies_time_std[week][:chop_time],
-                alpha=0.2, color='orange')
-
 # diffusion
-K_h = 10
+
+colors_diff = ['limegreen', 'darkgreen']
+ls_diff = ["-", (0, (5, 1, 1, 4, 1, 1))]
+
+K_h = 1000
 # Create logarithmically spaced indices
-# This will sample more points at the beginning and fewer at the end
-n_samples = 150  # Choose the number of points you want
+n_samples = 100  # Choose the number of points you want
 log_indices = np.unique(np.geomspace(1, len(time_range)-1, n_samples).astype(int))
 
 # Use these indices to sample your data
@@ -232,22 +190,83 @@ y_sampled = entropies_diff_mean[K_h][log_indices]
 # ax.plot(time_range[:], entropies_diff_mean[K_h][:],  
 #         label=r'$K_h = 10 \ m^2 s^{-1}$', linestyle=(0, (2, 2, 10, 2)) , linewidth=2, color=color_diff)
 
-ax.fill_between(time_range[:], entropies_diff_mean[K_h][:] - entropies_diff_std[K_h][:], 
-                entropies_diff_mean[K_h][:] + entropies_diff_std[K_h][:],
-                alpha=0.5, color='grey')
-ax.scatter(time_sampled, y_sampled,
-        label=r'$K_h = 10 \ m^2 s^{-1}$', color='k', marker=',', s=1)
+for j, K_h in enumerate([10, 1000]):
+    ax3.fill_between(time_range[:], entropies_diff_mean[K_h][:] - entropies_diff_std[K_h][:], 
+                    entropies_diff_mean[K_h][:] + entropies_diff_std[K_h][:],
+                    alpha=0.2, color=colors_diff[j])
+    # ax3.scatter(time_sampled, y_sampled,
+    #         label=rf'$K_h = {K_h}\ m^2 s^{-1}$', color=colors_diff[j], marker='^', s=10)
+    ax3.plot(time_range, entropies_diff_mean[K_h][:], linestyle=ls_diff[j], color=colors_diff[j],
+             label=rf'$K_h = {K_h:,}\ m^2 s^{-1}$')
+
+#reference
+ax3.plot(time_range, mixture_entropy_space[0.1], ls='-', color='black', label=r'Mixture: $\delta_r = 0.1^o$')
+
+ax3.set_xlim(1, 2189)
+ax3.set_ylim(0., 10.5)
+ax3.semilogx()
+ax3.legend(shadow=True, fontsize=10, loc='upper left')
+ax3.set_xlabel('Particle Age (days)')
+ax3.grid()
+ax3.text(0.95, 0.05, 'C', transform=ax3.transAxes, fontsize=16, fontweight='bold', va='bottom', ha='right')
 
 
-ax.plot(time_range, mixture_entropy_space[0.1], ls='-', color='black', label=r'Mixture: $\delta_r = 0.1^o$')
 
-ax.set_xlim(1, 2189)
-ax.set_ylim(0., 10.5)
+plt.tight_layout()
+plt.savefig('../figs/Fig4_Combined-Representation_entropy.png', dpi=300)
 
-ax.semilogx()
-ax.legend(shadow=True)
-ax.set_ylabel('Marginal Entropy (bits)')
-ax.set_xlabel('Particle Age (days)')
-ax.grid()
-# plt.savefig('../figs/Fig4-Temporal-Represen
-# %%
+# %% With diffusion
+
+# fig, ax = plt.subplots()
+
+# # Spatial plot
+# delta_r = 2.0
+# ax.plot(time_range, entropies_space_mean[delta_r],  
+#             label=f'$\delta_r = {delta_r}^o$', linestyle='-.', linewidth=2, color='teal')
+# ax.fill_between(time_range, entropies_space_mean[delta_r] - entropies_space_std[delta_r], 
+#                     entropies_space_mean[delta_r] + entropies_space_std[delta_r], 
+#                     alpha=0.2, color="teal")
+
+
+# # time
+# week = 20
+# chop_time = len(P_m['time'].values) - week*7
+# ax.plot(time_range[:chop_time], entropies_time_mean[week][:chop_time],  
+#         label=f'{week} weeks release', linestyle=(0, (3, 1, 1, 1, 1, 1)), linewidth=2, color='orange')
+# ax.fill_between(time_range[:chop_time], entropies_time_mean[week][:chop_time] - entropies_time_std[week][:chop_time], 
+#                 entropies_time_mean[week][:chop_time] + entropies_time_std[week][:chop_time],
+#                 alpha=0.2, color='orange')
+
+# # diffusion
+# K_h = 10
+# # Create logarithmically spaced indices
+# # This will sample more points at the beginning and fewer at the end
+# n_samples = 150  # Choose the number of points you want
+# log_indices = np.unique(np.geomspace(1, len(time_range)-1, n_samples).astype(int))
+
+# # Use these indices to sample your data
+# time_sampled = time_range[log_indices]
+# y_sampled = entropies_diff_mean[K_h][log_indices]
+
+# # ax.plot(time_range[:], entropies_diff_mean[K_h][:],  
+# #         label=r'$K_h = 10 \ m^2 s^{-1}$', linestyle=(0, (2, 2, 10, 2)) , linewidth=2, color=color_diff)
+
+# ax.fill_between(time_range[:], entropies_diff_mean[K_h][:] - entropies_diff_std[K_h][:], 
+#                 entropies_diff_mean[K_h][:] + entropies_diff_std[K_h][:],
+#                 alpha=0.5, color='grey')
+# ax.scatter(time_sampled, y_sampled,
+#         label=r'$K_h = 10 \ m^2 s^{-1}$', color='k', marker=',', s=1)
+
+
+# ax.plot(time_range, mixture_entropy_space[0.1], ls='-', color='black', label=r'Mixture: $\delta_r = 0.1^o$')
+
+# ax.set_xlim(1, 2189)
+# ax.set_ylim(0., 10.5)
+
+# ax.semilogx()
+# ax.legend(shadow=True)
+# ax.set_ylabel('Marginal Entropy (bits)')
+# ax.set_xlabel('Particle Age (days)')
+# ax.grid()
+# # plt.savefig('../figs/Fig4-Temporal-Represen
+# # %%

plot_scripts/fig5-6-Cross_entropy_in_time.py
@@ -62,17 +62,17 @@ N_members = 50
 KLD_ALL_mean = {}
 KLD_ALL_std = {}
 
-for delta_ref in [2., 4, 20]:
+for delta_ref in [0.1, 2., 4, 20]:
     
     KLDivergence_mean = {}
     KLDivergence_std = {}
     
-    for set in [0.1, 2., 4, 20, 'diff']:
+    for set in [0.1, 2., 4, 20, 10, 1000]:
         print(f'Processing set P: {delta_ref}, Q:{set}')
 
         _KLD = np.zeros((N_members**2, time_length))
 
-        for l, member in tqdm(enumerate(range(1, N_members+1))):
+        for l, member in enumerate(tqdm(range(1, N_members+1))):
             for subset_ref in range(1, N_members+1):
                 # Loading the Mixture reference distribution as subset_ref 
 
@@ -93,9 +93,9 @@ for delta_ref in [2., 4, 20]:
                 elif set in [4, 12, 20]:
                     # Temporal
                     file_path_Q = base_path + f"{location}_temporal_long/P_W{set:01d}_m{member:03d}.nc"
-                elif set == 'diff':
+                elif set in [10, 1000]:
                     # Diffusion
-                    file_path_Q = base_path + f"{location}_diffusion_long/P_diff_Kh_10_m{member:03d}.nc"
+                    file_path_Q = base_path + f"{location}_diffusion_long/P_diff_Kh_{set}_m{member:03d}.nc"
                 
                 P_Q = xr.open_dataset(file_path_Q)
                 P_Q = P_Q.sortby('hexint')
@@ -116,10 +116,10 @@ for delta_ref in [2., 4, 20]:
         KLDivergence_std[set] = std_KLD
 
 
-    with open(f'/Volumes/Claudio SSD/Ensemble_article_data/analysis/KLD_time/KLD_time_{delta_ref}{patch}.pkl', 'wb') as f:
+    with open(f'/Volumes/Claudio SSD/Ensemble_article_data/analysis/KLD_time/KLD_time_{delta_ref}{patch}_{set}.pkl', 'wb') as f:
         pickle.dump(KLDivergence_mean, f)
     
-    with open(f'/Volumes/Claudio SSD/Ensemble_article_data/analysis/KLD_time/KLD_time_{delta_ref}_std{patch}.pkl', 'wb') as f:
+    with open(f'/Volumes/Claudio SSD/Ensemble_article_data/analysis/KLD_time/KLD_time_{delta_ref}_std{patch}_{set}.pkl', 'wb') as f:
         pickle.dump(KLDivergence_std, f)
 
     KLD_ALL_mean[delta_ref] = KLDivergence_mean
@@ -142,6 +142,30 @@ for delta_ref in [0.1, 2., 4, 20]:
         KLD_ALL_std[delta_ref] = pickle.load(f)
 
 #%%
+KLD_ALL_mean_1000 = {}
+KLD_ALL_std_1000 = {}
+
+for delta_ref in [0.1, 2., 4, 20]:
+    path = f'/Volumes/Claudio SSD/Ensemble_article_data/analysis/KLD_time/KLD_time_{delta_ref}{patch}_1000.pkl'
+    
+    with open(path, 'rb') as f:
+        KLD_ALL_mean_1000[delta_ref] = pickle.load(f)
+        
+    path = f'/Volumes/Claudio SSD/Ensemble_article_data/analysis/KLD_time/KLD_time_{delta_ref}_std{patch}_1000.pkl'
+    
+    with open(path, 'rb') as f:
+        KLD_ALL_std_1000[delta_ref] = pickle.load(f)
+
+for delta_ref in [0.1, 2., 4, 20]:
+    KLD_ALL_mean[delta_ref][10] = KLD_ALL_mean[delta_ref].pop('diff')
+    KLD_ALL_mean[delta_ref][1000] = KLD_ALL_mean_1000[delta_ref][1000]
+
+    KLD_ALL_std[delta_ref][10] = KLD_ALL_std[delta_ref].pop('diff')
+    KLD_ALL_std[delta_ref][1000] = KLD_ALL_std_1000[delta_ref][1000]
+    
+
+
+#%%
 fig, axs = plt.subplots(2, 2, figsize=(9, 7), sharex=True, sharey=True)
 # fig.subplots_adjust(hspace=0.4, wspace=0.4)
 
@@ -152,17 +176,17 @@ axs = axs.ravel()
 
 for i, key in enumerate([0.1, 2., 4, 20]):
     
-    lss = [(0, (1, 1)), '-.', '--',(0, (3, 1, 1, 1, 1, 1)), '-']
-    colors = ['mediumblue', 'teal', 'darkred', 'orange', 'black']
+    lss = [(0, (1, 1)), '-.', '--', (0, (3, 1, 1, 1, 1, 1)), '-', (0, (5, 5))]
+    colors = ['mediumblue', 'teal', 'darkred', 'orange', 'black', 'purple']
     
     ax = axs[i]
-    for k, set in enumerate([0.1, 2., 4, 20, 'diff']):
+    for k, set in enumerate([0.1, 2., 4, 20, 10, 1000]):
         if set in [4, 12, 20]:
             labelz = f'{set:01d} weeks'
         elif set in [0.1, 1., 2.]:
             labelz = f'$\delta_r = {set}^o$'
-        else:
-            labelz = r'$K_h = 10 \ m^2 s^{-1}$'
+        elif set in [10, 1000]:
+            labelz = rf'$K_h = {set:,} \ m^2 s^{-1}$'
         
         ax.fill_between(time_range, KLD_ALL_mean[key][set] - KLD_ALL_std[key][set], 
                         KLD_ALL_mean[key][set] + KLD_ALL_std[key][set], alpha=0.3, 
@@ -178,7 +202,7 @@ for i, key in enumerate([0.1, 2., 4, 20]):
     ax.text(0.05, 0.9, f'$\mathbf{{{labels[i]}}}$  '+r'$P_{mix}$ : '+f'{labeltt}', fontsize=14, transform=ax.transAxes)
 
     ax.semilogx()
-    ax.legend(fontsize=8)
+    ax.legend(fontsize=6.5)
     ax.grid()
     ax.set_xlim(0, time_length)
     ax.set_ylim(0, 8)
@@ -195,9 +219,9 @@ plt.savefig(f'../figs/Fig5_relative_entropy_subplots{patch}.png', dpi=300)
 DF = {}
 
 for i, key in enumerate(KLD_ALL_mean.keys()):
-    _kld = np.zeros(5)
+    _kld = np.zeros(6)
     
-    for k, set in enumerate([0.1, 2., 4, 20, 'diff']):
+    for k, set in enumerate([0.1, 2., 4, 20, 10, 1000]):
         
         _kld[k] = np.mean(KLD_ALL_mean[key][set])
         print(f'key: {key}, set: {set}, kld: {_kld[k]}', type(_kld[k]))
@@ -210,19 +234,19 @@ for i, key in enumerate(KLD_ALL_mean.keys()):
     DF[labeltt] = _kld
 
 DF = pd.DataFrame(DF)
-DF.set_index([[r'$\delta_r = 0.1^o$', r'$\delta_r = 2^o$', '4 weeks', '20 weeks', r'$K_h = 10 \ m^2 s^{-1}$']], inplace=True)
+DF.set_index([[r'$\delta_r = 0.1^o$', r'$\delta_r = 2^o$', '4 weeks', '20 weeks', r'$K_h = 10 \ m^2 s^{-1}$', r'$K_h = 1,000 \ m^2 s^{-1}$']], inplace=True)
 
 # %% Kullback-Leibler divergence plot 
-fig, ax = plt.subplots(figsize=(6, 5))
+fig, ax = plt.subplots(figsize=(6.5, 5))
 
 cmmap = "Greens_r"
 
-sns.heatmap(DF, annot=True, fmt=".3f", cmap=cmmap, ax=ax, cbar=True, vmin=0)
+sns.heatmap(DF, annot=True, fmt=".3f", cmap=cmmap, ax=ax, cbar=True, vmin=-0.14)
 
 # Rotate y tick labels 90 degrees
-ax.set_yticklabels(ax.get_yticklabels(), rotation=90, ha='center', fontsize=9)
+ax.set_yticklabels(ax.get_yticklabels(), rotation=80, ha='center', fontsize=7)
 # Rotate x tick labels 15 degrees
-ax.set_xticklabels(ax.get_xticklabels(), rotation=15, ha='center', fontsize=9)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=15, ha='center', fontsize=7)
 
 # Add colorbar label
 cbar = ax.collections[0].colorbar